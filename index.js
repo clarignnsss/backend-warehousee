67dbd6801a34a83a5fafe05c607ccca7281e8a97
clarignnsss <reginanaclarissa@gmail.com>
2023-06-28 16:35:28 +0700

    New Update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import companyRouter from "../backend/router/companyRouter.js";
 import countryRouter from "../backend/router/countryRouter.js";
 import provinceRouter from "../backend/router/provinceRouter.js";
 import cityRouter from "../backend/router/cityRouter.js";
-import attributeRouter from "./router/product/attributeRouter.js";
+import attributeRouter from "../backend/router/product/attributeRouter.js";
 import attrValueRouter from "./router/product/attrValueRouter.js";
 import productAttributeRouter from "./router/product/productAttrRouter.js";
 import dotenv from "dotenv";
@@ -14,6 +14,7 @@ import cookieParser from "cookie-parser";
 import uomRouter from "./router/product/uomRouter.js";
 import productRouter from "./router/product/productRouter.js";
 import productUnitRouter from "./router/product/productUnitRouter.js";
+import warehouseRouter from "./router/product/warehouseRouter.js";
 
 dotenv.config();
 const app = express();
@@ -37,6 +38,7 @@ app.use("/api/productattr", productAttributeRouter);
 app.use("/api/uom", uomRouter);
 app.use("/api/product", productRouter);
 app.use("/api/productunit", productUnitRouter);
+app.use("/api/warehouse", warehouseRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("run on http://localhost:4000");
